5c9a2f8318bb603a7d5a1c37627f93a198b5d1a3
0liver4 <153389030+0liver4@users.noreply.github.com>
2025-09-12 17:58:39 -0400

    Aplicacion de las tipografias

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, useRoutes } from "react-router-dom"
 import routes from "~react-pages"
+import "./index.css"
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
+      <nav className="flex justify-end font-Darling">
         <a href="/">Inicio</a> |{" "}
         <a href="/about">Acerca</a> |{" "}
         <a href="/users">Usuarios</a> | {" "}
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,3 +1,5 @@
+
+
 export default function Home() {
   return <h1>Bienvenido a Coloreando Sueños</h1>
 }

13d470e87e256e55e2511379c5dd47e2748a1313
0liver4 <153389030+0liver4@users.noreply.github.com>
2026-02-07 11:27:44 -0400

    Header terminado (sin funcionamiento de botones y menu)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css';
-import  Header  from "./components/header/header";
+import Header from "./components/header/header";
+import Hero from './components/hero/hero';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
